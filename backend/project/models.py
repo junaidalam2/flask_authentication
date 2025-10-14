8e1c58689aed068d5c98b314948eb6f6c5b620e0
junaidalam2 <junaid.alam2@gmail.com>
2025-10-13 23:00:19 -0700

    updated model

---
+++ backend/project/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+class User(db.Model):
+    id = id.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
