a88c7400d3610c6b5b4e0be23b205e0cc87e53f5
junaidalam2 <junaid.alam2@gmail.com>
2025-10-13 22:54:09 -0700

    updated main auth templates config and flaskenv

--- backend/config.py
+++ backend/config.py
@@ -1,20 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import os
 
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DTABASE_URI'] = 'sqlite:///db.sqlite'
-
-    db.init_app(app)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key-goes-here")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # recommended for SQLAlchemy
