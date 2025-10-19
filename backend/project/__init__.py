39038135f773722b0d9cacbb9da04f725fb33e6f
junaidalam2 <junaid.alam2@gmail.com>
2025-10-18 23:22:27 -0700

    migrated user table

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
